9f7b5990bfc74cffc362591392a44e42e5c85b9e
Anshul Kuthe <145104113+anshulkuthe02@users.noreply.github.com>
2025-01-11 22:13:35 +0530

    Add files via upload

---
+++ problem11.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+elif num1 < num2:
+    print(f"{num2} is greater than {num1}")
+else:
+    print("Both are equal")
