ac74f694ddd0eca6a41de3bacc553bde6b9cf7ab
Anshul Kuthe <145104113+anshulkuthe02@users.noreply.github.com>
2025-01-17 00:33:22 +0530

    Add files via upload

---
+++ problem22.py
@@ -0,0 +1,5 @@
+ip = input("Enter the character:\n").lower()
+if ip == 'a' or ip == 'e' or ip == 'o' or ip == 'i' or ip == 'u':
+        print(f"The data entered is {ip} is a vowel.")
+else:
+        print(f"The data entered {ip} is a consonant")
