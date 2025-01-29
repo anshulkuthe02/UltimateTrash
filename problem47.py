38dca08b9512d9d2b6eb9a291b6e5f5872b4ae59
Anshul Kuthe <145104113+anshulkuthe02@users.noreply.github.com>
2025-01-30 00:07:30 +0530

    Add files via upload

---
+++ problem47.py
@@ -0,0 +1,9 @@
+def num(n1,n2):
+    squ = n1**2
+    squ2 = n2**2
+    total = squ + squ2
+    print(f"The total is: {total}")
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("enter num2: "))
+num(num1,num2)
