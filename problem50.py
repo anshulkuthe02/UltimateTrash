e6aa1449c5d7b805baa1b7dd7af8d8dfdda8b77a
Anshul Kuthe <145104113+anshulkuthe02@users.noreply.github.com>
2025-01-31 00:14:23 +0530

    Add files via upload

---
+++ problem50.py
@@ -0,0 +1,7 @@
+def area(base,height):
+    area_t = .5*base*height
+    print(f"Area is: {area_t}")
+
+base = float(input("Enter base: "))
+height = float(input("Enter Height: "))
+area(base,height)
